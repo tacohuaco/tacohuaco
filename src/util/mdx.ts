7c1719a9a85ca98f759dd2a564104f8a62f4eb87
Artem Sapegin <artem@sapegin.ru>
2021-08-04 16:35:05 +0200

    Don't break on empty Markdown fields

--- src/util/mdx.ts
+++ src/util/mdx.ts
@@ -3,7 +3,7 @@ import { transform } from 'sucrase';
 
 // Based on https://spectrum.chat/gatsby-js/general/rendering-content-to-page-with-frontmatter-data~4bda35b1-c923-4e96-ad89-5eb8bdd8dfec
 export const getMdx = async (source: string) => {
-	const code = await mdx(source);
+	const code = await mdx(source || '');
 	const result = transform(code, {
 		transforms: ['jsx'],
 		jsxPragma: 'mdx',
