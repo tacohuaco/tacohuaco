e533bb1bca1663c5d86f1dc79e99a3aa1ea3bf8e
Artem Sapegin <artem@sapegin.ru>
2021-08-09 17:59:45 +0200

    About page "fish" 🐟

---
+++ src/layouts/AboutPage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Stack, Heading } from 'tamia';
+import Metatags from '../components/Metatags';
+import Page from './Page';
+
+type Props = {
+	url: string;
+};
+
+export default function AboutPage({ url }: Props) {
+	return (
+		<Page url={url}>
+			<Metatags slug={url} />
+			<Stack as="main" gap="l">
+				<Heading level={1}>
+					About Tacohuaco <span aria-hidden="true">🌮</span>
+				</Heading>
+				<Stack gap="m">
+					<p>
+						Klats-klats! <span aria-hidden="true">🦀</span>
+					</p>
+				</Stack>
+			</Stack>
+		</Page>
+	);
+}
