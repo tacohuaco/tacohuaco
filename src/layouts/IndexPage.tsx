8952a68bb47aa9150c8d39ab0e8c4b8ca87f6f73
Artem Sapegin <artem@sapegin.ru>
2021-08-16 15:16:12 +0200

    Try and discard recipes for next season
    They are the same as for current month now and I'm not sure it will be often different.

--- src/layouts/IndexPage.tsx
+++ src/layouts/IndexPage.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { orderBy } from 'lodash';
-import { Stack, Heading } from 'tamia';
+import { Stack, Heading, VisuallyHidden } from 'tamia';
 import Page from './Page';
 import RecipeList from '../components/RecipeList';
 import Metatags from '../components/Metatags';
@@ -12,29 +12,53 @@ type Props = {
 };
 
 const CURRENT_SEASON = new Date().getMonth() + 1;
+// const NEXT_SEASON = CURRENT_SEASON === 12 ? 1 : CURRENT_SEASON + 1;
 
-// Order recipies by the lengths of the seasons: recipes with shorter season first
-const getSeasonalRecipes = (recipes: RecipeMeta[]): RecipeMeta[] => {
+// Order recipies by the lengths of the seasons: recipes with shorter season
+const getCurrentSeasonRecipes = (recipes: RecipeMeta[]): RecipeMeta[] => {
 	return orderBy(
 		recipes.filter((x) => x.seasons.includes(CURRENT_SEASON)),
 		(x) => x.seasons.length
 	);
 };
 
+// const getNextSeasonRecipes = (recipes: RecipeMeta[]): RecipeMeta[] => {
+// 	return orderBy(
+// 		recipes.filter((x) => x.seasons.includes(NEXT_SEASON)),
+// 		(x) => x.seasons.length
+// 	);
+// };
+
+const RecipeListSection = ({
+	title,
+	recipes,
+}: {
+	title: ReactNode;
+	recipes: RecipeMeta[];
+}) => (
+	<Stack as="main" gap="l">
+		<Heading as="h2" level={1}>
+			{title}
+		</Heading>
+		<RecipeList recipes={recipes} />
+	</Stack>
+);
+
 export default function RecipePage({ recipes, url }: Props) {
-	const seasonalRecipes = getSeasonalRecipes(recipes);
 	return (
 		<Page url={url}>
 			<Metatags slug={url} images={recipes?.[0].images} />
+			<VisuallyHidden as="h1">Recipes</VisuallyHidden>
 			<Stack as="main" gap="xl">
-				<Stack as="main" gap="l">
-					<Heading level={1}>Recipes with ingredients in season</Heading>
-					<RecipeList recipes={seasonalRecipes} />
-				</Stack>
-				<Stack as="main" gap="l">
-					<Heading level={1}>Recipes</Heading>
-					<RecipeList recipes={recipes} />
-				</Stack>
+				<RecipeListSection
+					title="Recipes with ingredients in season"
+					recipes={getCurrentSeasonRecipes(recipes)}
+				/>
+				{/* <RecipeListSection
+					title="Recipes for the next month"
+					recipes={getNextSeasonRecipes(recipes)}
+				/> */}
+				<RecipeListSection title="All recipes" recipes={recipes} />
 			</Stack>
 		</Page>
 	);
