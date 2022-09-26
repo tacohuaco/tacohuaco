882037150e24bbe1fd5af8f19d34742e3778e0dc
Artem Sapegin <artem@sapegin.ru>
2022-09-26 16:12:31 +0200

    Add links to Twitter and Instagram 🥨

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,11 @@ export function Footer() {
 				and&nbsp;<Link href="https://sapegin.me/">Artem Sapegin</Link>
 				{', '}
 				{new Date().getFullYear()}
-				{' • '}Made with The&nbsp;Secret Ingredient™ in&nbsp;Berlin{' • '}
+				{' • '}Made with The&nbsp;Secret Ingredient™ in&nbsp;Berlin
+				{' • '}Follow&nbsp;us on&nbsp;
+				<Link href="https://twitter.com/tacohuaco">Twitter</Link> and&nbsp;
+				<Link href="https://www.instagram.com/tacohuaco.co/">Instagram</Link>!
+				{' • '}
 				<Link href="https://www.buymeacoffee.com/sapegin">Buy us coffee</Link>
 				{' • '}
 				<Link href="/atom.xml">RSS</Link>
