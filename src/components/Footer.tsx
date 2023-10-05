8ddb0bb878a384ec37059f95970ec5bade065060
Artem Sapegin <artem@sapegin.ru>
2023-10-05 08:26:20 +0200

    Update Dr. Taco link 🌮

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,7 @@ export function Footer() {
 	return (
 		<footer>
 			<Text variant="xsmall" mb="xs">
-				©{' '}
-				<Link href="https://www.linkedin.com/in/dr-margarita-diaz-cortes-4544a513a/">
-					Dr.&nbsp;Margarita Diaz Cortes
-				</Link>{' '}
+				© <Link href="https://drtaco.net/">Dr.&nbsp;Margarita Diaz Cortes</Link>{' '}
 				and&nbsp;<Link href="https://sapegin.me/">Artem Sapegin</Link>
 				{', '}
 				{new Date().getFullYear()}
