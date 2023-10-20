de2c4459ec86a750a7ad863ee8f8343942a75b78
Artem Sapegin <artem@sapegin.ru>
2023-10-20 15:52:44 +0200

    Try to fix the build

--- src/components/CoffeeRecipe.tsx
+++ src/components/CoffeeRecipe.tsx
@@ -1,5 +1,4 @@
 import { Stack, Heading, Text } from '.';
-import parseMs from 'parse-ms';
 import {
 	type CoffeeRecipe as CoffeeRecipeType,
 	type Step,
@@ -7,12 +6,26 @@ import {
 } from '../util/cafe';
 import Group from 'react-group';
 
+// HACK: Importing this library breaks the build on Netlify
+// https://github.com/sindresorhus/parse-ms
+function parseMilliseconds(milliseconds: number) {
+	return {
+		days: Math.trunc(milliseconds / 86400000),
+		hours: Math.trunc(milliseconds / 3600000) % 24,
+		minutes: Math.trunc(milliseconds / 60000) % 60,
+		seconds: Math.trunc(milliseconds / 1000) % 60,
+		milliseconds: Math.trunc(milliseconds) % 1000,
+		microseconds: Math.trunc(milliseconds * 1000) % 1000,
+		nanoseconds: Math.trunc(milliseconds * 1e6) % 1000,
+	};
+}
+
 type Props = {
 	recipe: CoffeeRecipeType;
 };
 
 const formatDuration = (durationSec: number) => {
-	const { minutes, seconds } = parseMs(durationSec * 1000);
+	const { minutes, seconds } = parseMilliseconds(durationSec * 1000);
 	if (minutes) {
 		return `${minutes} minutes`;
 	}
