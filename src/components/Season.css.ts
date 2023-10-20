a13760d519b430382676ccefe11c24cdd08ee2e5
Artem Sapegin <artem@sapegin.ru>
2023-10-20 16:39:08 +0200

    Try to fix seasons
    Increase specificity because of the wrong file inclusion order in Astro build

--- src/components/Season.css.ts
+++ src/components/Season.css.ts
@@ -2,5 +2,11 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '../styles/theme.css';
 
 export const notInSeason = style({
-	color: vars.colors.light,
+	selectors: {
+		// HACK: Increase specificity because of the wrong file inclusion order
+		// in Astro build
+		'&&': {
+			color: vars.colors.light,
+		},
+	},
 });
