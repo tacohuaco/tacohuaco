005f2bdbf6d8092a8006efa56517544a8bf083ae
Artem Sapegin <artem@sapegin.ru>
2024-02-18 12:48:05 +0100

    Fix mock sync script 🦔

--- scripts/syncCi.ts
+++ scripts/syncCi.ts
@@ -1,5 +1,6 @@
 import fs from 'fs-extra';
 import { globSync } from 'glob';
+import { basename } from 'path';
 
 // Copy mock recipes and shops for tests on CI
 
@@ -7,13 +8,15 @@ console.log('🌮 Writing mock content...');
 
 fs.ensureDirSync('src/content/recipes');
 
+console.log();
 console.log('🍕 Writing mock recipes...');
 
 const recipeMocks = globSync('e2e/mocks/recipes/*.json');
 
 recipeMocks.forEach((file) => {
 	console.log('👉', file);
-	fs.copyFileSync(file, `src/content/recipes/${file}`);
+	const filename = basename(file);
+	fs.copyFileSync(file, `src/content/recipes/${filename}`);
 });
 
 console.log();
@@ -21,6 +24,7 @@ console.log('🌭 Writing mock shops...');
 
 fs.ensureDirSync('src/data');
 
+console.log('👉', `e2e/mocks/data/shops.json`);
 fs.copyFileSync(`e2e/mocks/data/shops.json`, `src/data/shops.json`);
 
 console.log();
