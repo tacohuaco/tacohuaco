05cea96c4425389d498b7585dd0227efa152111b
Artem Sapegin <artem@sapegin.ru>
2024-02-18 15:16:22 +0100

    Fix typo

--- src/util/olivier/langs/en/ingredients.ts
+++ src/util/olivier/langs/en/ingredients.ts
@@ -288,7 +288,7 @@ export const INGREDIENTS = [
 	/**
 	 * Meat and fish
 	 */
-	[['chicken broth', 'chicken broth', 'chicken stock'],
+	[['chicken broth', 'chicken broth', 'chicken stock']],
 	[['chicken leg', 'chicken legs']],
 	[['chicken thigh', 'chicken thighs']],
 	[['chicken breast', 'chicken breasts']],
