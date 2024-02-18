38e87bcc545e477527d2312fe1ab0834e4eb3a4a
Artem Sapegin <artem@sapegin.ru>
2024-02-18 12:44:06 +0100

    Add mock recipes and shops to run tests on CI

---
+++ scripts/syncCi.ts
@@ -0,0 +1,27 @@
+import fs from 'fs-extra';
+import { globSync } from 'glob';
+
+// Copy mock recipes and shops for tests on CI
+
+console.log('🌮 Writing mock content...');
+
+fs.ensureDirSync('src/content/recipes');
+
+console.log('🍕 Writing mock recipes...');
+
+const recipeMocks = globSync('e2e/mocks/recipes/*.json');
+
+recipeMocks.forEach((file) => {
+	console.log('👉', file);
+	fs.copyFileSync(`e2e/mocks/recipes/${file}`, `src/content/recipes/${file}`);
+});
+
+console.log();
+console.log('🌭 Writing mock shops...');
+
+fs.ensureDirSync('src/data');
+
+fs.copyFileSync(`e2e/mocks/data/shops.json`, `src/data/shops.json`);
+
+console.log();
+console.log(`🍆 Done`);
