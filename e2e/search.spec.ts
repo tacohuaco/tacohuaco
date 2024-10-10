c808161beb57c4a223ca6d71de6137a0e3bf7c1a
Artem Sapegin <artem@sapegin.ru>
2024-10-10 17:56:21 +0200

    Fix tests

--- e2e/search.spec.ts
+++ e2e/search.spec.ts
@@ -7,7 +7,24 @@ test('has title', async ({ page }) => {
 	await expect(page).toHaveTitle('Search recipes on Tacohuaco');
 });
 
-test('search for a recipe', async ({ page }) => {
+test('search for a recipe, ingredient', async ({ page }) => {
+	await page.goto('/search/');
+
+	const input = page.getByPlaceholder('Search recipes');
+
+	expect(input).toBeEnabled();
+
+	await input.fill('cucumbers');
+	await input.press('ArrowDown', { delay: 500 });
+	await input.press('Enter', { delay: 500 });
+
+	// The recipe card is visible
+	await expect(
+		page.getByRole('link', { name: 'Quick pickled cucumbers' })
+	).toBeVisible();
+});
+
+test('search for a recipe, recipe name', async ({ page }) => {
 	await page.goto('/search/');
 
 	const input = page.getByPlaceholder('Search recipes');
@@ -18,7 +35,8 @@ test('search for a recipe', async ({ page }) => {
 	await input.press('ArrowDown', { delay: 500 });
 	await input.press('Enter', { delay: 500 });
 
+	// We are on the recipe page
 	await expect(
-		page.getByRole('link', { name: 'Chicken birria' })
+		page.getByRole('heading', { name: 'Chicken birria', exact: true })
 	).toBeVisible();
 });
